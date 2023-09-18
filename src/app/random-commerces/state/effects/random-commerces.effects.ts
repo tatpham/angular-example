d2052a589aa51f092d8bfb9189c9341336abdbcd
Tat Thien <tat.pham@web.de>
2023-09-18 09:49:25 +0200

    Fix typing

--- src/app/random-commerces/state/effects/random-commerces.effects.ts
+++ src/app/random-commerces/state/effects/random-commerces.effects.ts
@@ -1,17 +1,18 @@
 // commerce.effects.ts
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
 
 import { loadRandomCommerces, loadRandomCommercesFailure, loadRandomCommercesSuccess } from '../actions/random-commerces.actions';
 import { RandomCommerceService } from 'src/app/services/random-commerce.service';
+import { Action } from '@ngrx/store';
 
 @Injectable()
 export class CommerceEffects {
   constructor(private actions$: Actions, private commerceService: RandomCommerceService) {}
 
-  loadCommerces$ = createEffect(() =>
+  loadCommerces$: Observable<Action> = createEffect(() =>
     this.actions$.pipe(
       ofType(loadRandomCommerces),
       switchMap(() =>
